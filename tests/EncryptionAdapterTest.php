9f9880a1a75234502f7deb5f6d6724479d0b80bf
Guillaume RODRIGUEZ <guiguidu31300@gmail.com>
2019-04-24 11:42:58 +0200

    Add some tests

--- tests/EncryptionAdapterTest.php
+++ tests/EncryptionAdapterTest.php
@@ -2,20 +2,96 @@
 
 namespace League\Flysystem\Encryption\Tests;
 
+use League\Flysystem\AdapterInterface;
+use League\Flysystem\Config;
+use League\Flysystem\Encryption\EncryptionAdapter;
+use ParagonIE\Halite\Alerts\CannotPerformOperation;
+use ParagonIE\Halite\Alerts\FileAccessDenied;
+use ParagonIE\Halite\Alerts\FileError;
+use ParagonIE\Halite\Alerts\FileModified;
+use ParagonIE\Halite\Alerts\InvalidKey;
+use ParagonIE\Halite\Alerts\InvalidType;
+use ParagonIE\Halite\Halite;
+use ParagonIE\Halite\KeyFactory;
+use ParagonIE\Halite\Stream\WeakReadOnlyFile;
+use ParagonIE\Halite\Symmetric\EncryptionKey;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
 
 /**
  * Class EncryptionAdapterTest
  */
-class EncryptionAdapterTest extends TestCase
+final class EncryptionAdapterTest extends TestCase
 {
+    /** @var EncryptionKey */
+    private $encryptionKey;
 
+    /**
+     * @throws CannotPerformOperation
+     * @throws InvalidKey
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->encryptionKey = KeyFactory::generateEncryptionKey();
+    }
+
+    /**
+     * @param $resource
+     *
+     * @return bool
+     *
+     * @throws CannotPerformOperation
+     * @throws FileAccessDenied
+     * @throws FileError
+     * @throws FileModified
+     * @throws InvalidType
+     */
+    public static function checkEncryptedStream($resource): bool
+    {
+        $file = new WeakReadOnlyFile($resource);
+        $file->reset();
+        $header = $file->readBytes(Halite::VERSION_TAG_LEN);
+
+        return \is_resource($resource) && \ord($header[0]) === 49 && \ord($header[1]) === 65;
+    }
+
+    /**
+     * @throws CannotPerformOperation
+     * @throws InvalidKey
+     */
     public function testWriteStream(): void
+    {
+        /** @var ObjectProphecy $prophecy */
+        $prophecy = $this->prophesize(AdapterInterface::class);
+        /** @var AdapterInterface $decoratedAdapter */
+        $decoratedAdapter = $prophecy->reveal();
+        $encryptionAdapter = new EncryptionAdapter($decoratedAdapter, $this->encryptionKey);
+
+        $tmpFile = $this->getTempFile();
+        $config = new Config();
+        $prophecy
+            ->writeStream('bar/foo.txt', Argument::that([self::class, 'checkEncryptedStream']), $config)
+            ->willReturn([])
+            ->shouldBeCalledTimes(1);
+
+        $result = $encryptionAdapter->writeStream('bar/foo.txt', $tmpFile, $config);
+        $this->assertIsArray($result, 'The adapter result is an array.');
+
+        if (\is_resource($tmpFile)) {
+            fclose($tmpFile);
+        }
+
+        $prophecy->checkProphecyMethodsPredictions();
+    }
+
+    public function testEncryptedReadStream(): void
     {
         $this->markTestSkipped('Not implemented yet.');
     }
 
-    public function testReadStream(): void
+    public function testUnencryptedReadStream(): void
     {
         $this->markTestSkipped('Not implemented yet.');
     }
@@ -30,7 +106,12 @@ class EncryptionAdapterTest extends TestCase
         $this->markTestSkipped('Not implemented yet.');
     }
 
-    public function testRead(): void
+    public function testEncryptedRead(): void
+    {
+        $this->markTestSkipped('Not implemented yet.');
+    }
+
+    public function testUnecryptedRead(): void
     {
         $this->markTestSkipped('Not implemented yet.');
     }
@@ -53,5 +134,35 @@ class EncryptionAdapterTest extends TestCase
     public function testGetMetadata(): void
     {
         $this->markTestSkipped('Not implemented yet.');
+    }
+
+    /**
+     * @return resource
+     */
+    private function getTempFile()
+    {
+        $size = (int)($this->getMemoryLimit() * 1.5);
+
+        $tmpFile = tmpfile();
+        fseek($tmpFile, $size);
+        fprintf($tmpFile, 'a');
+        fflush($tmpFile);
+        fseek($tmpFile, 0);
+
+        return $tmpFile;
+    }
+
+    /**
+     * @return int
+     */
+    private function getMemoryLimit(): int
+    {
+        return (int)preg_replace_callback(
+            '/^(\-?\d+)([BKMG]?)$/i',
+            static function ($match) {
+                return $match[1] * (1024 ** strpos('BKMG', $match[2]));
+            },
+            strtoupper(ini_get('memory_limit'))
+        );
     }
 }
